4087dbb732a91d50a85b6964c09d6b7e08047207
Thomas Hallock <thomas@1stdibs.com>
2015-10-13 20:02:02 -0400

    ensureStopped

--- control.js
+++ control.js
@@ -1,5 +1,8 @@
 var childProcess = require('child_process');
 var running = require('is-running');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var Promise = require('es6-promise').Promise;
 var Control = module.exports = function (config) {
     this._fs = config.fs || require('fs');
     this._config = config;
@@ -9,15 +12,68 @@ Control.prototype._startDaemon = function () {
         stdio: this._getDaemonStdio(),
         detached: true
     });
+    mkdirp.sync(path.dirname(this._config.pidpath), {fs: this._fs});
     this._fs.writeFileSync(this._config.pidpath, '' + child.pid);
     child.unref(); // now safe to exit the current process without killing the daemon
 };
 Control.prototype._getDaemonStdio = function () {
     return ['ignore', this._fs.openSync('./stdout.out', 'w'), this._fs.openSync('./stderr.out', 'w')];
 };
+
+Control.prototype._getDaemonPID = function () {
+    try {
+        return this._fs.readFileSync(this._config.pidpath, 'utf8');
+    } catch (e) {
+        if ('ENOENT' === e.code) {
+            return undefined;
+        }
+        throw e;
+    }
+};
+Control.prototype.ensureStopped = function () {
+    var daemonpid = this._getDaemonPID();
+    var forceKillTimeout;
+    var giveUpTimeout;
+    var pollInterval;
+    var clearAsyncStuff;
+    clearAsyncStuff = function () {
+        clearTimeout(giveUpTimeout);
+        clearTimeout(forceKillTimeout);
+        clearInterval(pollInterval);
+    };
+    return new Promise(function (success, reject) {
+        if (undefined === daemonpid) {
+            success(); // daemon never ran
+            return;
+        }
+        if (!running(daemonpid)) {
+            success();
+            return;
+        }
+        process.kill(daemonpid, 'SIGINT');
+        pollInterval = setInterval(function () {
+            if (!running(daemonpid)) {
+                clearAsyncStuff();
+                success();
+            }
+        }, 100);
+        forceKillTimeout = setTimeout(function () {
+            process.kill(daemonpid, 'SIGKILL');
+        }, 1000);
+        giveUpTimeout = setTimeout(function () {
+            reject("could not kill daemon");
+        }, 1500);
+    }).then(function () {
+        clearAsyncStuff();
+    }, function (err) {
+        clearAsyncStuff();
+        throw err;
+    });
+};
 Control.prototype.ensureRunning = function () {
-    var daemonpid = this._fs.readFileSync(this._config.pidpath, 'utf8');
-    if (running(daemonpid)) {
+    var daemonpid = this._getDaemonPID();
+    if (undefined !== daemonpid && running(daemonpid)) {
+        console.log('daemon for ' + this._config.daemonModulePath + ' is already running');
         return;
     }
     this._startDaemon(); // daemon is not running, so start it
--- test/control.js
+++ test/control.js
@@ -12,13 +12,23 @@ describe('daemon controller', function () {
     var mfs;
     var config;
     var daemonRunning;
+    var sandbox;
+    var garbagepid;
+    var goodpid;
     beforeEach(function () {
+        goodpid = '987654321';
+        garbagepid = '1234567890';
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(Control.prototype, '_getDaemonStdio', function () {return 'inherit';});
         daemonRunning = false;
         mfs = new MemoryFileSystem();
         config = {
             fs: mfs,
             pidpath: '/test/pid/path'
         };
+    });
+    afterEach(function () {
+        sandbox.restore();
     });
     describe('config.fs', function () {
         it('should override the default filesystem module', sinon.test(function () {
@@ -31,18 +41,122 @@ describe('daemon controller', function () {
             fsMock.verify();
         }));
     });
+    describe('ensureStopped', function () {
+        var control;
+        var numPolls;
+        var daemonRunning;
+        beforeEach(function () {
+            daemonRunning = true;
+            control = new Control(config);
+            numPolls = 0;
+            sandbox.stub(running, 'stub', function (pid) {
+                ++numPolls;
+                return daemonRunning;
+            });
+        });
+        it("should succeed even if the daemon isn't running", sinon.test(function (done) {
+            daemonRunning = false;
+            this.stub(Control.prototype, "_getDaemonPID", function () {return garbagepid;});
+            control.ensureStopped().then(function () {
+                done();
+            }, function (err) {
+                done(err);
+            });
+            this.clock.tick(1000);
+        }));
+        it("should succeed even if the daemon never ran", sinon.test(function (done) {
+            daemonRunning = false;
+            this.stub(Control.prototype, "_getDaemonPID", function () {return undefined;});
+            control.ensureStopped().then(function () {
+                done();
+            }, function (err) {
+                done(err);
+            });
+            this.clock.tick(1000);
+        }));
+        it("should start by sending SIGINT to the process", sinon.test(function (done) {
+            daemonRunning = true;
+            this.stub(Control.prototype, "_getDaemonPID", function () {return goodpid;});
+            this.stub(process, 'kill', function (pid, signal) {
+                assert.equal(signal, 'SIGINT');
+                assert.equal(pid, goodpid);
+                done();
+            });
+            control.ensureStopped();
+            daemonRunning = false;
+            this.clock.tick(1000);
+        }));
+        it("should not SIGKILL if SIGINT kills the process before 1000 ms", sinon.test(function (done) {
+            daemonRunning = true;
+            this.stub(Control.prototype, "_getDaemonPID", function () {return goodpid;});
+            var receivedSIGINT = false;
+            this.stub(process, 'kill', function (pid, signal) {
+                assert.equal(pid, goodpid);
+                assert.equal('SIGINT', signal);
+                receivedSIGINT = true;
+            });
+            control.ensureStopped().then(function () {
+                assert.equal(receivedSIGINT, true);
+                done();
+            }, function (err) {
+                done(err);
+            });
+            daemonRunning = false;
+            this.clock.tick(1000);
+        }));
+        it("should send SIGKILL if SIGINT fails to kill the process after 1000 ms", sinon.test(function (done) {
+            daemonRunning = true;
+            this.stub(Control.prototype, "_getDaemonPID", function () {return goodpid;});
+            var receivedSIGKILL = false;
+            this.stub(process, 'kill', function (pid, signal) {
+                if (signal === 'SIGKILL') {
+                    receivedSIGKILL = true;
+                    assert.equal(pid, goodpid);
+                }
+            });
+            control.ensureStopped().then(function () {
+                assert.equal(receivedSIGKILL, true);
+                done();
+            }, null).then(null, function (err) {
+                done(err);
+            });
+            this.clock.tick(1000);
+            daemonRunning = false;
+            this.clock.tick(999);
+        }));
+        it('should poll the process and resolve the deferred when it has exited', sinon.test(function (done) {
+            daemonRunning = true;
+            var receivedSIGINT = false;
+            var receivedSIGKILL = false;
+            this.stub(Control.prototype, "_getDaemonPID", function () {return goodpid;});
+            this.stub(process, 'kill', function (pid, signal) {
+                assert.equal(pid, goodpid);
+                if (signal === 'SIGKILL') {
+                    receivedSIGKILL = true;
+                } else if (signal === 'SIGKILL') {
+                    receivedSIGINT = true;
+                }
+            });
+            control.ensureStopped().then(function () {
+                assert.equal(numPolls, 5);
+                done();
+            }).then(null, function (err) {
+                done(err);
+            });
+            this.clock.tick(100);
+            this.clock.tick(100);
+            this.clock.tick(100);
+            daemonRunning = false;
+            this.clock.tick(100);
+        }));
+    });
     describe('ensureRunning', function () {
-        var sandbox;
         var control;
         var mock;
         beforeEach(function () {
-            sandbox = sinon.sandbox.create();
             mkdirp.sync(path.dirname(config.pidpath), {fs: mfs});
             control = new Control(config);
             mock = sandbox.mock(control);
-        });
-        afterEach(function () {
-            sandbox.restore();
         });
         it('should not start if the process with fakeDaemon.pid is running', function (done) {
             var fakeDaemon;
@@ -64,17 +178,30 @@ describe('daemon controller', function () {
             mock.verify();
         });
         describe('_startDaemon', function () {
-            it('should record the daemon process id when starting a new daemon', sinon.test(function () {
-                var goodpid = '987654321';
-                var garbagepid = '1234567890';
-                var daemonpid;
-                this.stub(childProcess, 'spawn', function () {
+            beforeEach(function () {
+                sandbox.stub(childProcess, 'spawn', function () {
                     return {
                         unref: function () {},
                         pid: goodpid
                     };
                 });
-                this.stub(control, '_getDaemonStdio', function () {return 'inherit'});
+            });
+            it('should create the containing directory for pidpath if it does not exist', function () {
+                mfs = config.fs = new MemoryFileSystem();
+                control = new Control(config);
+                var errorReadingNonExistentPath = false;
+                try {
+                    mfs.readFileSync(config.pidpath, 'utf8');
+                } catch(e) {
+                    errorReadingNonExistentPath = true;
+                }
+                assert.equal(errorReadingNonExistentPath, true, 'pidpath exists even though we reset the memory filesystem');
+                control.ensureRunning();
+                daemonpid = mfs.readFileSync(config.pidpath, 'utf8');
+                assert.equal(daemonpid, goodpid, 'daemon pid was not what we were expecting');
+            });
+            it('should record the daemon process id when starting a new daemon', sinon.test(function () {
+                var daemonpid;
                 mfs.writeFileSync(config.pidpath, garbagepid);
                 control.ensureRunning();
                 daemonpid = mfs.readFileSync(config.pidpath, 'utf8');
